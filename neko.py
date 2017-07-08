0d162a7fb3266d8cf252c2b003e367bad9f17755
ℭunt <cunt@cunt.cunt>
2017-07-07 23:32:23 -0400

    Updated to dpy rewrite + shard support

--- neko.py
+++ neko.py
@@ -7,7 +7,7 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 description = """
 I am a bot made to show nekos from nekos.life
 """
-bot = commands.Bot(command_prefix="!", description=description)
+bot = commands.AutoShardedBot(command_prefix="!", description=description)
 
 
  
@@ -17,6 +17,20 @@ async def on_ready():
     print('Username: ' + bot.user.name)
     print('ID: ' + bot.user.id)
     print('------')
+    guilds = str(len(bot.guilds))
+    users = str(len(set(bot.get_all_members())))
+    for x in range(bot.shard_count):
+        message = 'shard {}/{} | {} guilds | {} users'.format(x+1,bot.shard_count,guilds, users)
+        game = discord.Game(name=message)
+        print(message)
+        await bot.change_presence(game=game,shard_id=x)
+
+@bot.command()
+async def shard(ctx):
+    """Shard info."""
+    shard = ctx.message.guild.shard_id + 1
+    em = discord.Embed(title="Your Guild is on shard:", description='{}/{}'.format(shard,bot.shard_count),color=discord.Color.blue())
+    await ctx.channel.send(embed=em)
 
 class neko:
 
@@ -32,7 +46,7 @@ class neko:
 
         embed = discord.Embed(colour=discord.Colour.blue())
         embed.set_image(url=nekos['neko'])
-        await self.bot.say(embed=embed)
+        await ctx.send(embed=embed)
 
 
 n = neko(bot)
